a808dc052f84f1728bb35a5f581314dd48c269a6
Vasyl <vasylcode@gmail.com>
2023-07-22 01:35:20 -0600

    inspector blocks added

--- src/components/NoMatch.jsx
+++ src/components/NoMatch.jsx
@@ -11,7 +11,7 @@ export default function NoMatch() {
 	};
 
 	const handleButtonClick = () => {
-		navigate(`/${inputValue}`);
+		navigate(`/${inputValue}/explorer`);
 	};
 
 	return (
--- src/pages/Inspector.jsx
+++ src/pages/Inspector.jsx
@@ -2,15 +2,13 @@ import { useState } from "react";
 
 export default function Inspector() {
 	const buttons = [
-		{ id: 1, label: "Button 1" },
-		{ id: 2, label: "Button 2" },
-		// Add more buttons if needed
+		{ id: 1, block: "123" },
+		{ id: 2, block: "456" },
 	];
 
 	const contentData = [
 		{ id: 1, title: "Content 1", content: "This is the content for Button 1." },
 		{ id: 2, title: "Content 2", content: "This is the content for Button 2." },
-		// Add more content data for each button
 	];
 
 	const [activeButton, setActiveButton] = useState(buttons[0].id);
@@ -32,7 +30,7 @@ export default function Inspector() {
 
 const LeftSidebar = ({ buttons, activeButton, handleButtonClick }) => {
 	return (
-		<div className="w-1/4 overflow-y-auto h-screen my-8 mx-6">
+		<div className="w-1/4 overflow-y-auto h-screen overflow-hidden my-8 mx-6">
 			{buttons.map((button) => (
 				<button
 					key={button.id}
@@ -44,6 +42,267 @@ const LeftSidebar = ({ buttons, activeButton, handleButtonClick }) => {
 					{button.label}
 				</button>
 			))}
+			<div className="flex flex-col mr-5">
+				<figure className="flex flex-col items-center justify-center p-8 text-center bg-black rounded-xl dark:bg-gray-800 dark:border-gray-700">
+					<blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+						<h3 className="text-lg font-semibold text-white dark:text-white">Block #123</h3>
+					</blockquote>
+					<div className="flex flex-1">
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							init
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							op
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							msg
+						</button>
+					</div>
+				</figure>
+				<div class="mx-auto h-[50px] min-h-[1em] w-0.5 self-stretch bg-gray-700 dark:bg-neutral-100 opacity-100 dark:opacity-80"></div>
+				<figure className="flex flex-col items-center justify-center p-8 text-center bg-black rounded-xl dark:bg-gray-800 dark:border-gray-700">
+					<blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+						<h3 className="text-lg font-semibold text-white dark:text-white">Block #123</h3>
+					</blockquote>
+					<div className="flex flex-1">
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							init
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							op
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							msg
+						</button>
+					</div>
+				</figure>
+				<div class="mx-auto h-[50px] min-h-[1em] w-0.5 self-stretch bg-gray-700 dark:bg-neutral-100 opacity-100 dark:opacity-80"></div>
+				<figure className="flex flex-col items-center justify-center p-8 text-center bg-black rounded-xl dark:bg-gray-800 dark:border-gray-700">
+					<blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+						<h3 className="text-lg font-semibold text-white dark:text-white">Block #123</h3>
+					</blockquote>
+					<div className="flex flex-1">
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							init
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							op
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							msg
+						</button>
+					</div>
+				</figure>
+				<div class="mx-auto h-[50px] min-h-[1em] w-0.5 self-stretch bg-gray-700 dark:bg-neutral-100 opacity-100 dark:opacity-80"></div>
+				<figure className="flex flex-col items-center justify-center p-8 text-center bg-black rounded-xl dark:bg-gray-800 dark:border-gray-700">
+					<blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+						<h3 className="text-lg font-semibold text-white dark:text-white">Block #123</h3>
+					</blockquote>
+					<div className="flex flex-1">
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							init
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							op
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							msg
+						</button>
+					</div>
+				</figure>
+				<div class="mx-auto h-[50px] min-h-[1em] w-0.5 self-stretch bg-gray-700 dark:bg-neutral-100 opacity-100 dark:opacity-80"></div>
+				<figure className="flex flex-col items-center justify-center p-8 text-center bg-black rounded-xl dark:bg-gray-800 dark:border-gray-700">
+					<blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+						<h3 className="text-lg font-semibold text-white dark:text-white">Block #123</h3>
+					</blockquote>
+					<div className="flex flex-1">
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							init
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							op
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							msg
+						</button>
+					</div>
+				</figure>
+				<div class="mx-auto h-[50px] min-h-[1em] w-0.5 self-stretch bg-gray-700 dark:bg-neutral-100 opacity-100 dark:opacity-80"></div>
+				<figure className="flex flex-col items-center justify-center p-8 text-center bg-black rounded-xl dark:bg-gray-800 dark:border-gray-700">
+					<blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+						<h3 className="text-lg font-semibold text-white dark:text-white">Block #123</h3>
+					</blockquote>
+					<div className="flex flex-1">
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							init
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							op
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							msg
+						</button>
+					</div>
+				</figure>
+				<div class="mx-auto h-[50px] min-h-[1em] w-0.5 self-stretch bg-gray-700 dark:bg-neutral-100 opacity-100 dark:opacity-80"></div>
+				<figure className="flex flex-col items-center justify-center p-8 text-center bg-black rounded-xl dark:bg-gray-800 dark:border-gray-700">
+					<blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+						<h3 className="text-lg font-semibold text-white dark:text-white">Block #123</h3>
+					</blockquote>
+					<div className="flex flex-1">
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							init
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							op
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							msg
+						</button>
+					</div>
+				</figure>
+				<div class="mx-auto h-[50px] min-h-[1em] w-0.5 self-stretch bg-gray-700 dark:bg-neutral-100 opacity-100 dark:opacity-80"></div>
+				<figure className="flex flex-col items-center justify-center p-8 text-center bg-black rounded-xl dark:bg-gray-800 dark:border-gray-700">
+					<blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+						<h3 className="text-lg font-semibold text-white dark:text-white">Block #123</h3>
+					</blockquote>
+					<div className="flex flex-1">
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							init
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							op
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							msg
+						</button>
+					</div>
+				</figure>
+				<div class="mx-auto h-[50px] min-h-[1em] w-0.5 self-stretch bg-gray-700 dark:bg-neutral-100 opacity-100 dark:opacity-80"></div>
+				<figure className="flex flex-col items-center justify-center p-8 text-center bg-black rounded-xl dark:bg-gray-800 dark:border-gray-700">
+					<blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+						<h3 className="text-lg font-semibold text-white dark:text-white">Block #123</h3>
+					</blockquote>
+					<div className="flex flex-1">
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							init
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							op
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							msg
+						</button>
+					</div>
+				</figure>
+				<div class="mx-auto h-[50px] min-h-[1em] w-0.5 self-stretch bg-gray-700 dark:bg-neutral-100 opacity-100 dark:opacity-80"></div>
+				<figure className="flex flex-col items-center justify-center p-8 text-center bg-black rounded-xl dark:bg-gray-800 dark:border-gray-700">
+					<blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
+						<h3 className="text-lg font-semibold text-white dark:text-white">Block #123</h3>
+					</blockquote>
+					<div className="flex flex-1">
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							init
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							op
+						</button>
+						<button
+							type="button"
+							className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+						>
+							msg
+						</button>
+					</div>
+				</figure>
+			</div>
 		</div>
 	);
 };
@@ -59,6 +318,6 @@ const RightContent = ({ activeButtonContent }) => {
 
 const Divider = () => {
 	return (
-		<div class="h-[auto] min-h-[1em] w-px self-stretch bg-gradient-to-tr from-transparent via-neutral-500 to-transparent opacity-20 dark:opacity-100"></div>
+		<div className="h-[auto] min-h-[1em] w-px self-stretch bg-gradient-to-tr from-transparent via-neutral-500 to-transparent opacity-20 dark:opacity-100"></div>
 	);
 };
